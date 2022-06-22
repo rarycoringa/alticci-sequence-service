c67da9c48efb1447d2ae399ffb209813dbbd68d8
Rary Coringa <rarycoringa@gmail.com>
2022-06-22 17:44:48 -0300

    fix debug env var

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app import create_app
 if __name__ == "__main__":
     app = create_app()
 
-    debug = os.environ.get("DEBUG", True)
+    debug = os.environ.get("DEBUG", "TRUE")
     debug = True if debug.lower() == "true" else False
 
     app.run(host="0.0.0.0", port="8080", debug=debug)
